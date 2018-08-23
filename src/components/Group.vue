e6fead32785976840a41641e943ab66f0bb6e5b6
yangjiajia <yangjiajia@cookids.com.cn>
2018-08-23 15:03:48 +0800

    update

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,23 +1,46 @@
 <template>
 <div >
   <button >按钮2</button>
+  <div id="div" style="display: flex; background-color: cadetblue">
+    <div onclick="this.innerHTML = '改变'">云课堂</div>
+    <div>上课状态</div>
+    <div v-if="true">其他内容</div>
+  </div>
+  <!--@onmousemove="mOver(this)" @onmouseout="mOut(this)"this-->
 </div>
 </template>
 
 <script>
-  export default {
-    name: 'Group',
-    data () {
-      return {
-        loading: false
+export default {
+  data () {
+    return {
+      loading: false,
+      show: false
+    }
+  },
+  created () {
+    window.onload = function () {
+      var div = document.getElementById('div')
+      console.log('div', div)
+      div.onmouseout = function (event) {
+        console.log('onmouseout')
+        this.show = false
       }
+      div.onmouseover = function (event) {
+        console.log('onmouseover')
+        this.show = true
+      }
+    }
+  },
+  methods: {
+    mover (obj) {
+      this.show = true
+      console.log('鼠标移到上面了', obj)
     },
-    created () {
-
+    mout (obj) {
+      this.show = false
+      console.log('鼠标移走了', obj)
     }
   }
+}
 </script>
-
-<style scoped>
-
-</style>
